d0b942362efe2618d78eafb7026784284b6b1728
“shanghaijunheng” <78899500@qq.com>
2016-10-20 10:43:26 +0800

    Initial Commit

---
+++ 播放音效/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  播放音效
+//
+//  Created by 上海均衡 on 2016/10/20.
+//  Copyright © 2016年 上海均衡. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ 播放音效/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  播放音效
+//
+//  Created by 上海均衡 on 2016/10/20.
+//  Copyright © 2016年 上海均衡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
